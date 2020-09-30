dbea110fd521fdbd27838f60acd4d4e7fed7dbe2
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2020-09-30 13:39:42 +0200

    Implemented learning + relearning

--- topology-conservation.py
+++ topology-conservation.py
@@ -11,9 +11,10 @@ import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.patheffects as path_effects
 from matplotlib.collections import LineCollection, PolyCollection
-from spatial import blue_noise, voronoi, centroid
+from spatial import blue_noise, voronoi, centroid, clipped_voronoi
 
-
+from som import SOM
+from plot import weights_3D
 
 # -----------------------------------------------------------------------------
 if __name__ == '__main__':
@@ -36,7 +37,6 @@ if __name__ == '__main__':
     #  S0 : Edges
 
     P = blue_noise((1,1), radius=radius)
-    print("Number of neurons: {0} ({1})".format(len(P), n))
     for i in range(100): # Lloyd relaxation (100 iterations)
         V = voronoi(P, bbox=[0,1,0,1])
         C = []
@@ -46,6 +46,11 @@ if __name__ == '__main__':
         P = np.array(C)
     P0, V0 = P, V
 
+    # We reorder P0 such that last points are in bottom right corner
+    # It is not strictly necessary but it simplifies lesion code
+    # P0 = P0[np.argsort(P0[:,0] + (1-P0[:,1]))]
+    # V = voronoi(P0, bbox=[0,1,0,1])
+    
     # Computing connection matrix and edges
     D0 = scipy.spatial.distance.cdist(P0, P0)
     C0 = np.zeros(D0.shape, dtype=int)
@@ -56,22 +61,15 @@ if __name__ == '__main__':
             S0.append([P0[i], P0[j]])
 
     
-    fig = plt.figure( figsize=(18,6))
-    ax = plt.subplot(1,3,1, aspect=1)
-    ax.scatter(P0[:,0], P0[:,1], facecolor="white", edgecolor="black")
-    collection = LineCollection(S0, color="k",
-                                linewidth=1.5, zorder=-20, alpha=0.5)
-    ax.add_collection(collection)
-
-    
-            
     # Expanded distribution
     # -------------------------------
     #  P0 + SP : Initial state
     #  P1      : Final state
     n = 25
-    SP = np.random.uniform(0.50, 1.00, (n,2))
-    P = np.r_[P, SP]
+    SP = np.random.uniform(0.5, 1.00, (n,2))
+
+       
+    P = np.r_[P0, SP]
     for j in range(100): # Lloyd relaxation (100 iterations)
         V = voronoi(P, bbox=[0,1,0,1])
         for i,region in enumerate(V.filtered_regions):
@@ -120,18 +118,6 @@ if __name__ == '__main__':
                 S1.append([P1[i], P1[j]])
 
 
-                
-    ax = plt.subplot(1,3,2, aspect=1)
-    ax.scatter(P1[:len(P0),0], P1[:len(P0),1],
-               facecolor="white", edgecolor="black")
-    ax.scatter(P1[len(P0):,0], P1[len(P0):,1],
-               facecolor="black", edgecolor="black")
-    collection = LineCollection(S1, color="k",
-                                linewidth=1.5, zorder=-20, alpha=0.5)
-    ax.add_collection(collection)
-
-
-
     # Lesioned distribution
     # -------------------------------
     #  P0[:-n] : Initial state
@@ -168,65 +154,7 @@ if __name__ == '__main__':
             if count == n_neighbour:
                 break
                 
-    ax = plt.subplot(1,3,3, aspect=1)
-    ax.scatter(P2[:,0], P2[:,1], facecolor="white", edgecolor="black")
-    collection = LineCollection(S2, color="k",
-                                linewidth=1.5, zorder=-20, alpha=0.5)
-    ax.add_collection(collection)
-    plt.show()
-
-
     
-    # fig = plt.figure( figsize=(8,8))
-    # ax = plt.subplot(1,1,1, aspect=1)
-    # ax.scatter(P1[:,0], P1[:,1], color="C0")
-    # # ax.scatter(SP[:,0], SP[:,1], color="C1")
-    # segments = []
-    # for p0,p1 in zip( np.r_[P0, SP], P1):
-    #     segments.append([p0,p1])
-    # collection = LineCollection(segments, color="k", linewidth=1.5, zorder=-20)
-    # ax.add_collection(collection)
-    # # ax = plt.subplot(1,2,2, aspect=1)
-    # # ax.scatter(P1[:len(P0),0], P1[:len(P0),1], color="C0")
-    # # ax.scatter(P1[len(P0):,0], P1[len(P0):,1], color="C1")
-    # plt.show()
-
-
-    
-    """
-    n = 25
-    
-    for i in range(100):
-        V = voronoi(P, bbox=[0,1,0,1])
-        C = []
-        for region in V.filtered_regions:
-            vertices = V.vertices[region + [region[0]], :]
-            C.append(centroid(vertices))
-        P = np.array(C)
-    P0, V0 = P, V
-
-    SP = np.random.uniform(0.50, 1.00, (n,2))
-    P = np.r_[P, SP]
-    for j in range(100):
-        V = voronoi(P, bbox=[0,1,0,1])
-        for i,region in enumerate(V.filtered_regions):
-            vertices = V.vertices[region + [region[0]], :]
-            C = centroid(vertices)
-            P[np.argmin(((P-C)**2).sum(axis=1))] = C
-    P1, V1 = P, V
-
-
-    P = P0[:-n].copy()
-    # P = np.delete(P0, np.random.randint(0,len(P0),n), axis=0)
-    for j in range(100):
-        V = voronoi(P, bbox=[0,1,0,1])
-        for i,region in enumerate(V.filtered_regions):
-            vertices = V.vertices[region + [region[0]], :]
-            C = centroid(vertices)
-            P[np.argmin(((P-C)**2).sum(axis=1))] = C
-    P2, V2 = P, V
-
-
     print("A: {0} neurons".format(len(P0)))
     print("B: {0} neurons".format(len(P1)))
     print("C: {0} neurons".format(len(P2)))
@@ -234,11 +162,64 @@ if __name__ == '__main__':
 
 
     # -------------------------------------------------------------------------
-    fig = plt.figure(figsize=(15,10))
+    n_epochs   = 25000
+    sigma      = 0.50, 0.1
+    lrate      = 0.50, 0.2
+    X,Y = np.random.uniform(0, 1, (50000,3)), None
+    som_0 = SOM(size=len(P0), topology="random", neighbours=2, PVC = (P0, V0, C0))
+    som_0.voronoi = clipped_voronoi(P0, bbox=[0,1,0,1])
+    som_0.fit(X, Y, n_epochs, sigma=sigma, lrate=lrate)
 
+    n_epochs   = 5000
+    sigma      = 0.1, 0.01
+    lrate      = 0.2, 0.01
 
+    som_1 = SOM(size=len(P1), topology="random", neighbours=2, PVC = (P1, V1, C1))
+    som_1.voronoi = clipped_voronoi(P1, bbox=[0,1,0,1])
+    som_1.fit(X, Y, n_epochs, sigma=sigma, lrate=lrate, codebook=som_0.codebook)
+    
+    som_2 = SOM(size=len(P2), topology="random", neighbours=2, PVC = (P2, V2, C2))
+    som_2.voronoi = clipped_voronoi(P2, bbox=[0,1,0,1])
+    som_2.fit(X, Y, n_epochs, sigma=sigma, lrate=lrate, codebook=som_0.codebook)
+
+    som_0.fit(X, Y, n_epochs, sigma=sigma, lrate=lrate, codebook=som_0.codebook)
+        
     # -------------------------------------------------------------------------
-    ax = plt.subplot(2, 3, 1, aspect=1)
+    fig = plt.figure(figsize=(15,15), dpi=50)
+    nrow, ncol = 3, 3
+
+
+    ax = plt.subplot(nrow, ncol,  7, aspect=1)
+    weights_3D(ax, som_0)
+    text = ax.text(0.05, 0.05, "G",
+                   fontsize=32, fontweight="bold", transform=ax.transAxes)
+    text.set_path_effects([path_effects.Stroke(linewidth=2, foreground='white'),
+                           path_effects.Normal()])
+    ax.set_xlim(0,1), ax.set_ylim(0,1)
+    ax.set_xticks([]), ax.set_yticks([])
+
+    
+    ax = plt.subplot(nrow, ncol,  8, aspect=1)
+    weights_3D(ax, som_1)
+    text = ax.text(0.05, 0.05, "H",
+                   fontsize=32, fontweight="bold", transform=ax.transAxes)
+    text.set_path_effects([path_effects.Stroke(linewidth=2, foreground='white'),
+                           path_effects.Normal()])
+    ax.set_xlim(0,1), ax.set_ylim(0,1)
+    ax.set_xticks([]), ax.set_yticks([])
+
+    ax = plt.subplot(nrow, ncol,  9, aspect=1)
+    weights_3D(ax, som_2)
+    text = ax.text(0.05, 0.05, "I",
+                   fontsize=32, fontweight="bold", transform=ax.transAxes)
+    text.set_path_effects([path_effects.Stroke(linewidth=2, foreground='white'),
+                           path_effects.Normal()])
+    ax.set_xlim(0,1), ax.set_ylim(0,1)
+    ax.set_xticks([]), ax.set_yticks([])
+
+    
+    # --- A ---
+    ax = plt.subplot(nrow, ncol,  1, aspect=1)
     X, Y = P0[:,0], P0[:,1]
     FC = np.zeros((len(P0),4))
     FC[:] = 1,1,1,1
@@ -247,11 +228,12 @@ if __name__ == '__main__':
     EC[:] = 0,0,0,1
     EC[-n:] = 1,0,0,1
 
-    ax.scatter(X, Y, s=50, edgecolor=EC, facecolor=FC, linewidth=1.5)
+    ax.scatter(X[:10], Y[:10], s=10,
+               edgecolor="None", facecolor="black", linewidth=0,zorder=100)
 
+    ax.scatter(X, Y, s=50, edgecolor=EC, facecolor=FC, linewidth=1.5)
     ax.scatter(SP[:,0], SP[:,1],
                s=50, edgecolor="k", facecolor="k", linewidth=1.5)
-    
     segments = []
     for region in V0.filtered_regions:
         segments.append(V0.vertices[region + [region[0]], :])
@@ -264,18 +246,12 @@ if __name__ == '__main__':
     text.set_path_effects([path_effects.Stroke(linewidth=2, foreground='white'),
                            path_effects.Normal()])
 
-    # -------------------------------------------------------------------------
-    ax = plt.subplot(2, 3, 4, aspect=1)
-    D = scipy.spatial.distance.cdist(P0,P0)
-    sources = np.repeat(np.arange(len(P0)),n_neighbour).reshape(len(P0),n_neighbour)
-    targets = np.argsort(D,axis=1)[:,1:n_neighbour+1]
-    edges = np.c_[sources.ravel(), targets.ravel()]
+    # --- D ---
+    ax = plt.subplot(nrow, ncol,  4, aspect=1)
     X, Y = P0[:,0], P0[:,1]
     ax.scatter(X, Y, s=50, edgecolor="k", facecolor="w", linewidth=1.5)
-    segments = np.zeros((len(edges), 2, 2))
-    for i in range(len(edges)):
-        segments[i] = P0[edges[i,0]], P0[edges[i,1]]
-    collection = LineCollection(segments, color="k", zorder=-10, lw=1.5)
+    collection = LineCollection(S0, color="black",
+                                linewidth=1.5, zorder=-10, alpha=0.5)
     ax.add_collection(collection)
     ax.set_xlim(0,1), ax.set_ylim(0,1)
     ax.set_xticks([]), ax.set_yticks([])
@@ -284,10 +260,14 @@ if __name__ == '__main__':
     text.set_path_effects([path_effects.Stroke(linewidth=2, foreground='white'),
                            path_effects.Normal()])
 
-    
-    # -------------------------------------------------------------------------
-    ax = plt.subplot(2, 3, 2, aspect=1)
+
+    # --- B ---
+    ax = plt.subplot(nrow, ncol,  2, aspect=1)
     X, Y = P1[:,0], P1[:,1]
+
+    ax.scatter(X[:10], Y[:10], s=10,
+               edgecolor="None", facecolor="black", linewidth=0,zorder=100)
+    
     FC = np.zeros((len(P1),4))
     FC[:] = 1,1,1,1
     FC[-n:] = 0,0,0,1
@@ -306,6 +286,9 @@ if __name__ == '__main__':
         segments.append([p0,p1])
     collection = LineCollection(segments, color="k", linewidth=1.5, zorder=-20)
     ax.add_collection(collection)
+    P = np.r_[P0, SP]
+    ax.scatter(P[:,0], P[:,1], s=10, lw=0, zorder=-30,
+               edgecolor="None", facecolor="black")
     ax.set_xlim(0,1), ax.set_ylim(0,1)
     ax.set_xticks([]), ax.set_yticks([])
     text = ax.text(0.05, 0.05, "B",
@@ -313,20 +296,12 @@ if __name__ == '__main__':
     text.set_path_effects([path_effects.Stroke(linewidth=2, foreground='white'),
                            path_effects.Normal()])
 
-
-    # -------------------------------------------------------------------------
-    ax = plt.subplot(2, 3, 5, aspect=1)
-    D = scipy.spatial.distance.cdist(P1,P1)
-    sources = np.repeat(np.arange(len(P1)),n_neighbour).reshape(len(P1),n_neighbour)
-    targets = np.argsort(D,axis=1)[:,1:n_neighbour+1]
-    edges = np.c_[sources.ravel(), targets.ravel()]
-    
+    # --- E ---
+    ax = plt.subplot(nrow, ncol,  5, aspect=1)
     X, Y = P1[:,0], P1[:,1]
     ax.scatter(X, Y, s=50, edgecolor="k", facecolor="w", linewidth=1.5)
-    segments = np.zeros((len(edges), 2, 2))
-    for i in range(len(edges)):
-        segments[i] = P1[edges[i,0]], P1[edges[i,1]]
-    collection = LineCollection(segments, color="k", zorder=-10, lw=1.5)
+    collection = LineCollection(S1, color="black",
+                                linewidth=1.5, zorder=-10, alpha=0.5)
     ax.add_collection(collection)
     ax.set_xlim(0,1), ax.set_ylim(0,1)
     ax.set_xticks([]), ax.set_yticks([])
@@ -335,11 +310,14 @@ if __name__ == '__main__':
     text.set_path_effects([path_effects.Stroke(linewidth=2, foreground='white'),
                            path_effects.Normal()])
 
-
-
-    # -------------------------------------------------------------------------
-    ax = plt.subplot(2, 3, 3, aspect=1)
+    # --- C ---
+    ax = plt.subplot(nrow, ncol,  3, aspect=1)
     X, Y = P2[:,0], P2[:,1]
+
+    ax.scatter(X[:10], Y[:10], s=10,
+               edgecolor="None", facecolor="black", linewidth=0,zorder=100)
+
+    
     ax.scatter(X, Y, s=50, edgecolor="k", facecolor="w", linewidth=1.5)
     segments = []
     for region in V2.filtered_regions:
@@ -350,6 +328,9 @@ if __name__ == '__main__':
     segments = []
     for p0,p2 in zip(P0[:-n], P2):
         segments.append([p0,p2])
+    P = P0[:-n]
+    ax.scatter(P[:,0], P[:,1], s=10, lw=0, zorder=-30,
+               edgecolor="None", facecolor="black")
     collection = LineCollection(segments, color="k", linewidth=1.5, zorder=-20)
     ax.add_collection(collection)
     ax.set_xlim(0,1), ax.set_ylim(0,1)
@@ -360,18 +341,12 @@ if __name__ == '__main__':
                            path_effects.Normal()])
 
 
-    # -------------------------------------------------------------------------
-    ax = plt.subplot(2, 3, 6, aspect=1)
-    D = scipy.spatial.distance.cdist(P2,P2)
-    sources = np.repeat(np.arange(len(P2)),n_neighbour).reshape(len(P2),n_neighbour)
-    targets = np.argsort(D,axis=1)[:,1:n_neighbour+1]
-    edges = np.c_[sources.ravel(), targets.ravel()]
+    # --- F ---
+    ax = plt.subplot(nrow, ncol,  6, aspect=1)
     X, Y = P2[:,0], P2[:,1]
     ax.scatter(X, Y, s=50, edgecolor="k", facecolor="w", linewidth=1.5)
-    segments = np.zeros((len(edges), 2, 2))
-    for i in range(len(edges)):
-        segments[i] = P2[edges[i,0]], P2[edges[i,1]]
-    collection = LineCollection(segments, color="k", zorder=-10, lw=1.5)
+    collection = LineCollection(S2, color="black",
+                                linewidth=1.5, zorder=-10, alpha=0.5)
     ax.add_collection(collection)
     ax.set_xlim(0,1), ax.set_ylim(0,1)
     ax.set_xticks([]), ax.set_yticks([])
@@ -382,6 +357,5 @@ if __name__ == '__main__':
 
     
     plt.tight_layout()
-    plt.savefig("vsom-resilience.pdf")
+    plt.savefig("topology-conservation.pdf", dpi=300)
     plt.show()
-    """    
